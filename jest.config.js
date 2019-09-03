e2f3949487a6a6981acb7ffb2ea3ae8c24ba435c
julianjca <julianjca11@gmail.com>
2019-09-03 13:01:50 +0700

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,5 @@ module.exports = {
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.js',
   testResultsProcessor: 'jest-sonar-reporter',
+  reporters: ['default', 'jest-sonar'],
 };
